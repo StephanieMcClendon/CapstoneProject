5ca2ff52ddd3aeebeebcca1182f81c8dfdd170ed
Jordan Loudis <16jloudis@gmail.com>
2022-09-27 16:23:58 -0400

    first commit

---
+++ Backend/src/main/java/com/xpanxion/capstone/CapstoneApplication.java
@@ -0,0 +1,13 @@
+package com.xpanxion.capstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/xpanxion/capstone/CapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xpanxion.capstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
