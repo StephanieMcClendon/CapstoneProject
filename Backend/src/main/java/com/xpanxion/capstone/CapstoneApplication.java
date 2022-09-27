1fb83caef8babab711452ace02a1d779e8942f78
Jordan Loudis <16jloudis@gmail.com>
2022-09-27 17:14:03 -0400

    made user mode, repo and controller

--- Backend/src/main/java/com/xpanxion/capstone/CapstoneApplication.java
+++ Backend/src/main/java/com/xpanxion/capstone/CapstoneApplication.java
@@ -1,13 +1,13 @@
 package com.xpanxion.capstone;
 
+import com.xpanxion.capstone.model.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CapstoneApplication {
-
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(CapstoneApplication.class, args);
 	}
-
 }
