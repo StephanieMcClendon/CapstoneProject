302e5d5cb63e430ed101f3bb5269d13639efe338
Jordan Loudis <16jloudis@gmail.com>
2022-09-28 10:36:16 -0400

    Created create and read api for user model

--- Backend/src/main/java/com/xpanxion/capstone/controller/UserController.java
+++ Backend/src/main/java/com/xpanxion/capstone/controller/UserController.java
@@ -1,8 +1,32 @@
 package com.xpanxion.capstone.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.xpanxion.capstone.model.User;
+import com.xpanxion.capstone.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
 public class UserController
 {
+    private final UserRepository userRepository;
 
+    @Autowired
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @PostMapping("/user")
+    public User createUser(@RequestBody User user)
+    {
+        return this.userRepository.save(user);
+    }
+
+    @GetMapping("/user")
+    public List<User> getAllUsers()
+    {
+        return this.userRepository.findAll();
+    }
 }
