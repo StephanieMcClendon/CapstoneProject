4952c421c61c56a100eb32e4c7a56d9d90d424ad
Jordan Loudis <16jloudis@gmail.com>
2022-10-03 12:24:05 -0400

    fixed some endpoints for api and change angular routing component

--- Backend/src/main/java/com/xpanxion/capstone/controller/GoalController.java
+++ Backend/src/main/java/com/xpanxion/capstone/controller/GoalController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:4200")
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class GoalController {
     @Autowired
     private GoalRepository goalRepository;
@@ -23,7 +23,7 @@ public class GoalController {
         return this.goalRepository.findAll();
     }
 
-    @GetMapping("/goal/{id}")
+    @GetMapping("/goals/{id}")
     public Goal listGoal(@PathVariable Long id){
         return this.goalRepository.findById(id).get();
     }
@@ -41,7 +41,7 @@ public class GoalController {
     }
 
     // link to user id
-    @PutMapping("/update-goal/{id}")
+    @PutMapping("/goals/{id}")
     public Goal updateGoal(@RequestBody Goal inputGoal, @PathVariable Long id){
         Goal goal = this.goalRepository.findById(id).get();
         goal.setGoalAmount(inputGoal.getGoalAmount());
