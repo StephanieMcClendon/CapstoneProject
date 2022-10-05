de4c55cf16cff7a1d52527f0c1ce5594c3090abf
Tariq <tarsabree@gmail.com>
2022-10-05 09:06:21 -0400

    update to dashboard

--- Backend/src/main/java/com/xpanxion/capstone/controller/IncomeController.java
+++ Backend/src/main/java/com/xpanxion/capstone/controller/IncomeController.java
@@ -12,7 +12,7 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/v1")
-@CrossOrigin(origins = "http://localhost:4200")
+
 public class IncomeController
 {
     private final IncomeRepository incomeRepository;
